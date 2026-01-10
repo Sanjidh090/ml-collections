eb8ce3e6d63f17e1d0f18d12b950cd253b4203c3
Sanjid Hasan <155468281+Sanjidh090@users.noreply.github.com>
2026-01-10 19:21:46 +0600

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,6 +1,12 @@
+%pip install --upgrade tensorflow
+%pip install --upgrade protobuf
 import numpy as nnp
 import pandas as pd
 import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # Suppresses INFO and WARNING messages
+import warnings
+warnings.filterwarnings("ignore")
 # Loop through all directories and filenames in the input folder
 for dirname, _, filenames in os.walk('/kaggle/input'):
     # Print the first 10 filenames
